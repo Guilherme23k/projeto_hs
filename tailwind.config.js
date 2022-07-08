90be2d83ce1f725157d6ae414c4876ef8f6e9e89
Guilherme Ramos <guilhermeggr23@gmail.com>
2022-07-08 11:45:21 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+
+      backgroundImage: {
+        'fundo' : "url('/img/FUNTOTESTE.jpg')"
+      },
+      colors: {
+        'blue-header' : '#092a49',
+        'grey-flex' : '#333',
+        'whitesmoke' : '#f5f5f5'
+      },
+      fontFamily: {
+        'arial' : 'Arial',
+        'roboto': 'Roboto',
+        'teste' : 'Comic Sans MS',
+        'logo' : 'Heebo',
+        'lato' : 'Lato'
+      },
+      maxHeight: {
+        '57' : '14.375rem'
+      }
+    },
+  },
+  plugins: [],
+}
